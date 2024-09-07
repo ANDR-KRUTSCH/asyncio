d6379cfaac6ffb3d9394d3a5990d571dc99f606e
Andrew Krutsch <andr.krutsch@gmail.com>
2024-09-07 11:08:52 +0500

    Python Concurrency With AsyncIO - Chapter 7.2

--- main.py
+++ main.py
@@ -1,37 +1,21 @@
-import threading, socket
-from socket import socket as Socket
+import asyncio, requests, time
 
 
-class ClientEchoThread(threading.Thread):
+async def main() -> None:
+    loop = asyncio.get_running_loop(); tasks = list()
 
-    def __init__(self, client: Socket) -> None:
-        super().__init__()
-        self.client = client
+    start = time.time()
 
-    def run(self) -> None:
-        try:
-            while True:
-                if not (data := self.client.recv(2048)): raise BrokenPipeError('Connection closed!')
-                else: print(f'Received: {data}'); self.client.sendall(data)
-        except OSError as error: print(f'Thread was interrupt by exception: {error}')
+    for _ in range(10): tasks.append(asyncio.to_thread(lambda url: requests.get(url=url).status_code, 'https://www.example.com'))
 
-    def close(self) -> None:
-        if self.is_alive(): self.client.sendall(b'Stopping!'); self.client.shutdown(socket.SHUT_RDWR)
+    result = await asyncio.gather(*tasks)
 
-with Socket(family=socket.AF_INET, type=socket.SOCK_STREAM) as server:
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print(result)
 
-    server.bind(('127.0.0.1', 8000))
-    
-    server.listen()
+    end = time.time()
 
-    connection_threads: list[ClientEchoThread] = list()
+    print(f'{end - start:.4f}')
 
-    try:
-        while True:
-            connection, address = server.accept()
-            connection_threads.append((thread := ClientEchoThread(client=connection)))
-            thread.start()
-    except KeyboardInterrupt:
-        print('Stopping!')
-        [thread.close() for thread in connection_threads]
+
+if __name__ == '__main__':
+    asyncio.run(main=main())
