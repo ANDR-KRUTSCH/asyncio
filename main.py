f915a56d073338d76c3478f7cdc91c912f67c120
Andrew Krutsch <andr.krutsch@gmail.com>
2024-09-07 10:41:17 +0500

    Python Concurrency With AsyncIO - Chapter 7.1

--- main.py
+++ main.py
@@ -1,105 +1,37 @@
-import os, pathlib, time, functools, asyncio
-from concurrent.futures import ProcessPoolExecutor
-from multiprocessing.sharedctypes import Value
-from multiprocessing.sharedctypes import Synchronized
-from typing import Generator
+import threading, socket
+from socket import socket as Socket
 
 
-def delete_file():
-    os.remove(path=pathlib.Path(__file__).resolve().parent / 'digits.txt')
+class ClientEchoThread(threading.Thread):
 
-def create_file(number_of_rows: int) -> None:
+    def __init__(self, client: Socket) -> None:
+        super().__init__()
+        self.client = client
+
+    def run(self) -> None:
+        try:
+            while True:
+                if not (data := self.client.recv(2048)): raise BrokenPipeError('Connection closed!')
+                else: print(f'Received: {data}'); self.client.sendall(data)
+        except OSError as error: print(f'Thread was interrupt by exception: {error}')
+
+    def close(self) -> None:
+        if self.is_alive(): self.client.sendall(b'Stopping!'); self.client.shutdown(socket.SHUT_RDWR)
+
+with Socket(family=socket.AF_INET, type=socket.SOCK_STREAM) as server:
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server.bind(('127.0.0.1', 8000))
+    
+    server.listen()
+
+    connection_threads: list[ClientEchoThread] = list()
+
     try:
-        with open(file='digits.txt', mode='x') as file:
-            rows = [f'{row}\t{row}\t{row}\t{row}\n' for row in range(1, number_of_rows + 1)]
-            file.writelines(rows)
-    except FileExistsError:
-        delete_file()
-        create_file(number_of_rows=number_of_rows)
-
-map_progress: Synchronized
-
-def init(progress: Synchronized) -> None:
-    global map_progress
-    map_progress = progress
-
-def partition(data: list[str], chunk_size: int) -> Generator:
-    for _ in range(0, len(data), chunk_size): yield data[_ : _ + chunk_size]
-
-def map_frequencies(chunk: list[str]) -> dict[str, int]:
-    counter = dict()
-    for line in chunk:
-        word, _, count, _ = line.replace('\n', '').split('\t')
-        if counter.get(word):
-            counter[word] = counter[word] + int(count)
-        else:
-            counter[word] = int(count)
-
-    with map_progress.get_lock():
-        map_progress.value += 1
-
-    return counter
-
-def progress_reporter(total_partitions: int) -> None:
-    while map_progress.value < total_partitions:
-        print(f'Finished: {map_progress.value}/{total_partitions}')
-        time.sleep(1)
-    print(f'Finished: {map_progress.value}/{total_partitions}')
-
-# async def progress_reporter(total_partitions: int) -> None:
-#     while map_progress.value < total_partitions:
-#         print(f'Finished: {map_progress.value}/{total_partitions}')
-#         await asyncio.sleep(1)
-#     print(f'Finished: {map_progress.value}/{total_partitions}')
-
-def merge_dicts(first: dict[str, int], second: dict[str, int]) -> dict[str, int]:
-    merge = first
-    for key in second:
-        if key in merge:
-            merge[key] = merge[key] + second[key]
-        else:
-            merge[key] = second[key]
-    return merge
-
-async def main(partition_size: int) -> None:
-    create_file(number_of_rows=10000000)
-    
-    global map_progress
-
-    with open(file='digits.txt') as file:
-        contents = file.readlines()
-
-        map_progress = Value('i', 0)
-        tasks = list()
-        loop = asyncio.get_running_loop()
-
-        start = time.time()
-
-        with ProcessPoolExecutor(initializer=init, initargs=(map_progress, )) as pool:
-            total_partitions = len(contents) // partition_size
-
-            tasks.append(loop.run_in_executor(executor=pool, func=functools.partial(progress_reporter, total_partitions)))
-
-            # reporter = asyncio.create_task(coro=progress_reporter(total_partitions=total_partitions))
-            
-            for chunk in partition(data=contents, chunk_size=partition_size): tasks.append(loop.run_in_executor(executor=pool, func=functools.partial(map_frequencies, chunk)))
-
-        counters = await asyncio.gather(*tasks)
-        
-        counters.pop(counters.index(None))
-
-        # await reporter
-
-        final_result = functools.reduce(merge_dicts, counters)
-
-        print('60: {}'.format(final_result['60']))
-
-        end = time.time()
-
-        print(f'{end - start:.3f}')
-    
-    # delete_file()
-
-
-if __name__ == '__main__':
-    asyncio.run(main=main(partition_size=500000))
+        while True:
+            connection, address = server.accept()
+            connection_threads.append((thread := ClientEchoThread(client=connection)))
+            thread.start()
+    except KeyboardInterrupt:
+        print('Stopping!')
+        [thread.close() for thread in connection_threads]
