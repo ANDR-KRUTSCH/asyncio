11ac00ded8b5af0c99e8342fc518d765f2aaefb7
Andrew Krutsch <andr.krutsch@gmail.com>
2024-09-10 13:17:10 +0500

    Python Concurrency With AsyncIO - Chapter 8.5

--- main.py
+++ main.py
@@ -1,35 +1,50 @@
-import asyncio, os, tty, sys
-from collections import deque as Deque
-from util import MessageStore, reader, select_last_line, select_first_line, clear_selected_line, create_stdin_reader
+import asyncio, logging
+from asyncio import StreamReader, StreamWriter
+
+
+class ServerState:
+
+    def __init__(self) -> None:
+        self._writers: list[StreamWriter] = list()
+
+    async def add_client(self, reader: StreamReader, writer: StreamWriter) -> None:
+        self._writers.append(writer)
+        await self._on_connect(writer=writer)
+        asyncio.create_task(coro=self._echo(reader=reader, writer=writer))
+
+    async def _on_connect(self, writer: StreamWriter) -> None:
+        writer.write(data=f'Welcome! Current clients numbers: {len(self._writers)}.\n'.encode()); await writer.drain()
+        await self._notify_all(message='The new client has been connected.\n')
+
+    async def _echo(self, reader: StreamReader, writer: StreamWriter) -> None:
+        try:
+            while (data := await reader.readline()) != b'':
+                writer.write(data=data); await writer.drain()
+            self._writers.remove(writer)
+            await self._notify_all(message=f'The client has been disconnected. Current clients number: {len(self._writers)}')
+        except BaseException as error:
+            logging.exception(msg='Error reading the data from the client.', exc_info=error)
+            self._writers.remove(writer)
+
+    async def _notify_all(self, message: str) -> None:
+        for writer in self._writers:
+            try:
+                writer.write(data=message.encode()); await writer.drain()
+            except ConnectionError as error:
+                logging.exception(msg='Error writing the data to the client.', exc_info=error)
+                self._writers.remove(writer)
 
 
 async def main() -> None:
-    os.system(command='clear')
-    tty.setcbreak(fd=sys.stdin.fileno())
+    server_state = ServerState()
 
-    max_size = select_last_line()
+    async def client_connected(reader: StreamReader, writer: StreamWriter) -> None:
+        await server_state.add_client(reader=reader, writer=writer)
 
-    async def sleep(delay: int, message_store: MessageStore) -> None:
-        await message_store.append(message=f'Sleeping for {delay} second(s) started...')
-        await asyncio.sleep(delay=delay)
-        await message_store.append(message=f'Sleeping for {delay} second(s) finished.')
+    server = await asyncio.start_server(client_connected_cb=client_connected, host='127.0.0.1', port=8000)
 
-    async def update_stdout(deque: Deque) -> None:
-        select_first_line(max_size=max_size)
-
-        for message in deque:
-            print(message)
-
-        select_last_line()
-        clear_selected_line()
-
-    message_store = MessageStore(callback=update_stdout, max_size=max_size)
-
-    stdin_reader = await create_stdin_reader()
-
-    while True:
-        delay = await reader(stdin_reader=stdin_reader)
-        asyncio.create_task(coro=sleep(delay=delay, message_store=message_store))
+    async with server:
+        await server.serve_forever()
 
 
 if __name__ == '__main__':
