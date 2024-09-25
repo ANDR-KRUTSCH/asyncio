6862fbcb87d2ff6f1cd97ff7e7d37308edc4f9e7
Andrew Krutsch <andr.krutsch@gmail.com>
2024-09-25 13:21:29 +0500

    Python Concurrency With AsyncIO - Chapter 12.2

--- queues.py
+++ queues.py
@@ -15,36 +15,51 @@ class Customer:
         self.products = products
 
 
-async def checkout_customer(queue: asyncio.Queue, id: int) -> None:
-    while not queue.empty():
-        customer: Customer = queue.get_nowait()
-        print(f'cashier({id}) - customer({customer.id})')
+async def checkout_customer(queue: asyncio.Queue, id) -> None:
+    while True:
+        customer: Customer = await queue.get()
 
+        print(f'Cashier({id}) - Customer({customer.id})')
         for product in customer.products:
-            print(f'cashier({id}) - customer({customer.id}): {product.name}')
+            print(f'Cashier({id}) checkouts Customer({customer.id}): {product.name}')
             await asyncio.sleep(delay=product.checkout_time)
+            print(f'Cashier({id}) finished checkouting Customer({customer.id})')
         
-        print(f'cashier({id}) - customer({customer.id}):\t\t\tDone')
         queue.task_done()
 
-
-async def main() -> None:
-    queue = asyncio.Queue()
-
+def generate_customer(id: int) -> Customer:
     products_all = [
-        Product(name='Bread', checkout_time=0.1),
-        Product(name='Butter', checkout_time=0.2),
-        Product(name='Milk', checkout_time=0.3),
-        Product(name='Coffee', checkout_time=0.4),
+        Product(name='Milk', checkout_time=1),
+        Product(name='Coffee', checkout_time=2),
+        Product(name='Sugar', checkout_time=3),
     ]
 
-    for customer_id in range(10):
-        products = [products_all[random.randrange(len(products_all))] for _ in range(random.randrange(10))]
-        queue.put_nowait(Customer(id=customer_id, products=products))
-    
-    cashiers = [asyncio.create_task(coro=checkout_customer(queue=queue, id=cashier_id)) for cashier_id in range(3)]
+    products = [products_all[random.randrange(len(products_all))] for _ in range(random.randrange(10))]
 
-    await asyncio.gather(queue.join(), *cashiers)
+    return Customer(id=id, products=products)
+
+async def customer_generator(queue: asyncio.Queue) -> None:
+    total_customers = 0
+
+    while True:
+        customers = [generate_customer(id=id) for id in range(total_customers, total_customers + random.randrange(10))]
+
+        for customer in customers:
+            print('Waiting for adding the customer to the queue.')
+            await queue.put(customer)
+            print('The customer was added to the queue.')
+
+        total_customers + len(customers)
+        await asyncio.sleep(delay=1)
+
+async def main() -> None:
+    queue = asyncio.Queue(maxsize=5)
+
+    customer_producer = asyncio.create_task(coro=customer_generator(queue=queue))
+
+    cashiers = [asyncio.create_task(coro=checkout_customer(queue=queue, id=id)) for id in range(3)]
+
+    await asyncio.gather(customer_producer, *cashiers)
 
 
 if __name__ == '__main__':
