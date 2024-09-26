88960b7d0c18970de0f4f3335592de89767711a3
Andrew Krutsch <andr.krutsch@gmail.com>
2024-09-26 10:00:21 +0500

    Python Concurrency With AsyncIO - Chapter 13.2

--- subprocesses.py
+++ subprocesses.py
@@ -3,10 +3,11 @@ from asyncio.subprocess import Process
 
 
 async def main() -> None:
-    process: Process = await asyncio.create_subprocess_exec('ls', '-l')
-    print(f'PID: {process.pid}')
-    print('status:', await process.wait())
-
+    process: Process = await asyncio.create_subprocess_exec('sleep', '3')
+    try: print('status:', await asyncio.wait_for(fut=process.wait(), timeout=1))
+    except TimeoutError:
+        process.terminate()
+        print('status:', await process.wait())
 
 if __name__ == '__main__':
     asyncio.run(main=main())
