460f86c7b56a71036d70b633fae8c27cfaccd26f
Andrew Krutsch <andr.krutsch@gmail.com>
2024-09-26 10:38:46 +0500

    Python Concurrency With AsyncIO - Chapter 13.3

--- subprocesses.py
+++ subprocesses.py
@@ -1,13 +1,18 @@
 import asyncio
-from asyncio.subprocess import Process
+from asyncio.subprocess import Process, PIPE
 
+
+async def write_output(prefix: str, stdout: asyncio.StreamReader) -> None:
+    while line := await stdout.readline():
+        print(f'[{prefix}]: {line.rstrip().decode()}')
 
 async def main() -> None:
-    process: Process = await asyncio.create_subprocess_exec('sleep', '3')
-    try: print('status:', await asyncio.wait_for(fut=process.wait(), timeout=1))
-    except TimeoutError:
-        process.terminate()
-        print('status:', await process.wait())
+    process: Process = await asyncio.create_subprocess_exec('ls', '-l', stdout=PIPE)
+    stdout_task = asyncio.create_task(coro=write_output(prefix='ls -l', stdout=process.stdout))
+
+    code, _ = await asyncio.gather(process.wait(), stdout_task)
+    print('status:', code)
+
 
 if __name__ == '__main__':
     asyncio.run(main=main())
