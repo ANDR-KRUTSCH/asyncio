0e03a0bc7bc5c366d6afd7ee3516cea6dd36d3b0
Andrew Krutsch <andr.krutsch@gmail.com>
2024-09-26 12:08:50 +0500

    Python Concurrency With AsyncIO - Chapter 13.7

--- programm.py
+++ programm.py
@@ -1,3 +1,9 @@
+import random, time
+
+
 # print('Username:', input('Username: '))
 
-while (data := input('-> ')) != 'quit': print(data)
+while (data := input('-> ')) != 'quit':
+    for _ in range(random.randrange(10)):
+        time.sleep(0.5)
+        print(data)
--- subprocesses.py
+++ subprocesses.py
@@ -2,17 +2,25 @@ import asyncio
 from asyncio.subprocess import Process, PIPE
 
 
-async def consume_and_send(texts: list[str], stdin: asyncio.StreamWriter, stdout: asyncio.StreamReader) -> None:
+async def stdout_reader(stdout: asyncio.StreamReader, input_ready_event: asyncio.Event) -> None:
+    while (data := await stdout.read(1024)) != b'':
+        print(data)
+        if data.decode().endswith('-> '): input_ready_event.set()
+
+async def stdin_writer(stdin: asyncio.StreamWriter, input_ready_event: asyncio.Event, texts: list[str]) -> None:
     for text in texts:
-        print(await stdout.read(2048))
-        stdin.write(text.encode()); await stdin.drain()
+        await input_ready_event.wait()
+        stdin.write(data=text.encode()); await stdin.drain()
+        input_ready_event.clear()
 
 async def main() -> None:
     process: Process = await asyncio.create_subprocess_exec('python', 'programm.py', stdin=PIPE, stdout=PIPE)
 
+    input_ready_event = asyncio.Event()
+
     texts = ['one\n', 'two\n', 'three\n', 'quit\n']
 
-    await asyncio.gather(consume_and_send(texts=texts, stdout=process.stdout, stdin=process.stdin), process.wait())
+    await asyncio.gather(stdout_reader(stdout=process.stdout, input_ready_event=input_ready_event), stdin_writer(stdin=process.stdin, input_ready_event=input_ready_event, texts=texts), process.wait())
 
 
 if __name__ == '__main__':
