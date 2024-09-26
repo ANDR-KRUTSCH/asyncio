800b7aab45d7ed4b0204148d9fe630569c6cea2e
Andrew Krutsch <andr.krutsch@gmail.com>
2024-09-26 11:31:40 +0500

    Python Concurrency With AsyncIO - Chapter 13.5

--- subprocesses.py
+++ subprocesses.py
@@ -1,15 +1,31 @@
-import asyncio
+import asyncio, random, string, os, time
 from asyncio.subprocess import Process, PIPE
 
 
+async def encrypt(semaphore: asyncio.Semaphore, text: str) -> bytes:
+    programm = ['gpg', '-c', '--batch', '--passphrase', '3ncryptm3', '--cipher-algo', 'TWOFISH'] # gpg -c --batch --passphrase 3ncryptm3 --cipher-algo TWOFISH
+
+    async with semaphore:
+        process: Process = await asyncio.create_subprocess_exec(*programm, stdin=PIPE, stdout=PIPE)
+
+        stdout, stderr = await process.communicate(input=text.encode())
+
+        return stdout
+
 async def main() -> None:
-    process: Process = await asyncio.create_subprocess_exec('python', 'temp.py', stdout=PIPE)
+    texts = [''.join(random.choice(string.ascii_letters) for _ in range(1000)) for _ in range(1000)]
 
-    stdout, stderr = await process.communicate()
+    semaphore = asyncio.Semaphore(value=os.cpu_count())
 
-    print(stdout); print(stderr)
+    start = time.time()
 
-    print('status:', process.returncode)
+    tasks = [asyncio.create_task(coro=encrypt(semaphore=semaphore, text=text)) for text in texts]
+
+    await asyncio.gather(*tasks)
+
+    end = time.time()
+
+    print(f'time: {end-start:.4f} second(s)')
 
 
 if __name__ == '__main__':
