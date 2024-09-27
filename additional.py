ff395263feca666e76a9b6ddc5c13cffc573f9f4
Andrew Krutsch <andr.krutsch@gmail.com>
2024-09-27 10:01:33 +0500

    Python Concurrency With AsyncIO - Chapter 14.3

--- additional.py
+++ additional.py
@@ -1,27 +1,22 @@
-import asyncio, contextvars
+import asyncio
+from util import delay
 
 
-class Server:
-    users: contextvars.ContextVar = contextvars.ContextVar('users')
+async def create_task_no_sleep() -> None:
+    task_1 = asyncio.create_task(coro=delay(delay_seconds=1))
+    task_2 = asyncio.create_task(coro=delay(delay_seconds=2))
 
-    def __init__(self, host: str, port: int) -> None:
-        self.host: str = host
-        self.port: int = port
+    await asyncio.gather(task_1, task_2)
 
-    async def start_server(self) -> None:
-        server = await asyncio.start_server(client_connected_cb=self._client_connected, host=self.host, port=self.port)
-        await server.serve_forever()
-
-    def _client_connected(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
-        self.users.set(writer.get_extra_info(name='peername'))
-        asyncio.create_task(coro=self.listen_for_messages(reader=reader))
-
-    async def listen_for_messages(self, reader: asyncio.StreamReader) -> None:
-        while data := await reader.readline(): print(f'{self.users.get()} -> {data}')
-
+async def create_task_sleep() -> None:
+    task_1 = asyncio.create_task(coro=delay(delay_seconds=1))
+    await asyncio.sleep(delay=0)
+    task_2 = asyncio.create_task(coro=delay(delay_seconds=2))
+    await asyncio.sleep(delay=0)
+    await asyncio.gather(task_1, task_2)
 
 async def main() -> None:
-    await Server(host='127.0.0.1', port=8000).start_server()
+    await create_task_sleep()
 
 
 if __name__ == '__main__':
