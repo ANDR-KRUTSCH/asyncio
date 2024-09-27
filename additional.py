1669ffa9871ef1054a5efbd39ed891a9bd7f20d5
Andrew Krutsch <andr.krutsch@gmail.com>
2024-09-27 10:48:51 +0500

    Python Concurrency With AsyncIO - Chapter 14.4

--- additional.py
+++ additional.py
@@ -1,23 +1,29 @@
-import asyncio
-from util import delay
+import typing
 
 
-async def create_task_no_sleep() -> None:
-    task_1 = asyncio.create_task(coro=delay(delay_seconds=1))
-    task_2 = asyncio.create_task(coro=delay(delay_seconds=2))
-
-    await asyncio.gather(task_1, task_2)
-
-async def create_task_sleep() -> None:
-    task_1 = asyncio.create_task(coro=delay(delay_seconds=1))
-    await asyncio.sleep(delay=0)
-    task_2 = asyncio.create_task(coro=delay(delay_seconds=2))
-    await asyncio.sleep(delay=0)
-    await asyncio.gather(task_1, task_2)
-
-async def main() -> None:
-    await create_task_sleep()
+def generator(start: int, end: int) -> typing.Generator:
+    if not isinstance(start, int) and not isinstance(end, int): raise TypeError()
+    if start < 0 or (end < 0 or end < start): raise ValueError()
+    
+    for _ in range(start, end):
+        yield _
 
 
-if __name__ == '__main__':
-    asyncio.run(main=main())
+from_1_to_5 = generator(start=1, end=5)
+from_5_to_10 = generator(start=5, end=10)
+
+def run_generator_step(generator: typing.Generator):
+    try:
+        return generator.send(None)
+    except StopIteration as stop_iteration:
+        return stop_iteration.value
+
+while True:
+    from_1_to_5_result = run_generator_step(generator=from_1_to_5)
+    from_5_to_10_result = run_generator_step(generator=from_5_to_10)
+
+    print(from_1_to_5_result) if from_1_to_5_result is not None else ...
+    print(from_5_to_10_result) if from_5_to_10_result is not None else ...
+
+    if from_1_to_5_result is None and from_5_to_10_result is None:
+        break
